ad08df2a8d1e8ab27d0420d9e2d2ee50f1a8ae8c
ifalfahri <ifaltampan69@gmail.com>
2025-04-13 00:18:03 +0800

    feat: add HeroScroll component with dynamic title and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Bento } from "@/components/bento";
 import { GeminiSection } from "@/components/gemini-section";
 import RibbonSection from "@/components/ribbon-section";
+import { HeroScroll } from "@/components/hero-scroll";
 
 export default function Home() {
   return (
-    <div>
+    <div className="px-4 md:px-10 mx-auto max-w-screen">
+      <HeroScroll />
       <RibbonSection />
       <Bento />
       <GeminiSection />
---
+++ src/components/hero-scroll.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "./ui/container-scroll-animation";
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+
+export function HeroScroll() {
+  return (
+    <div className="flex flex-col overflow-hidden relative z-1">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <Badge variant="outline" className="-top-4 py-1 px-3 font-medium relative gap-1.5 rounded-full bg-gradient-to-r from-emerald-500/30 to-emerald-500/20">
+              <span
+                className="size-1.5 rounded-full bg-emerald-500"
+                aria-hidden="true"
+              ></span>
+             Terverifikasi Malas
+            </Badge>
+            <h1 className="text-xl relative md:text-4xl font-semibold text-black dark:text-white">
+              Jadilah Programmer Handal <br />
+              <span className="left-3/7 top-2/6 md:top-1/4 rotate-6 absolute bg-primary text-sm md:text-3xl text-white pb-1 md:pb-2 px-2 md:px-3">yang</span>
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Enggan Ngoding
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/banner.jpeg`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover max-w-auto h-full object-center"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
